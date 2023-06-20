606053a248dafb9599ae6c0c12dc7526b991fd89
levinceotieno <susankimemia@gmail.com>
2023-06-20 22:09:38 +0300

    this commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  */
 void print_alphabet(void)
 {
-char a; 
-for (a = 'a'; a <= 'z'; a++) 
-{ 
-_putchar(a); 
-} 
-_putchar('\n'); 
-return; 
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+return;
 }
 
