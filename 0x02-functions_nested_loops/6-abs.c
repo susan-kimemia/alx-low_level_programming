a8e857a2f8a179f0a15b095b923c6d9001cae324
levinceotieno <susankimemia@gmail.com>
2023-06-20 23:41:24 +0300

    this commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - checks for the absolute value of an integer
+ * @r: integer input
+ * Description: returns the absolute value of a number
+ * Return: absolute value
+ */
+int _abs(int r)
+{
+if (r >= 0)
+{
+return (r);
+}
+return (-r);
+}
+
